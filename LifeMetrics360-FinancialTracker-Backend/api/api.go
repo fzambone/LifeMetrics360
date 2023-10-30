79feac6147b25bcc6d2f88f9d5db433766de3f20
Fernando Zambone <fernando.zambone@gmail.com>
2023-10-30 19:48:16 -0300

    Refactoring database connection approach, improving error handling and improving overall code quality.

--- LifeMetrics360-FinancialTracker-Backend/api/api.go
+++ LifeMetrics360-FinancialTracker-Backend/api/api.go
@@ -3,47 +3,67 @@ package api
 import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"net/http"
 )
 
 type Expense struct {
-	ID       int     `json:"id"`
-	Amount   float64 `json:"amount"`
-	Category string  `json:"category"`
-	Merchant string  `json:"merchant"`
-	Date     string  `json:"date"`
+	ID       primitive.ObjectID `bson:"_id" json:"id"`
+	Amount   float64            `json:"amount"`
+	Category string             `json:"category"`
+	Merchant string             `json:"merchant"`
+	Date     string             `json:"date"`
+}
+
+// Custom JSON Marshaling for Expense Struct
+func (e *Expense) MarshalJSON() ([]byte, error) {
+	type Alias Expense
+	return json.Marshal(&struct {
+		ID string `json:"id"`
+		*Alias
+	}{
+		ID:    e.ID.Hex(),
+		Alias: (*Alias)(e),
+	})
+}
+
+// Custom JSON Unmarshaling for Expense Struct
+func (e *Expense) UnmarshalJSON(data []byte) error {
+	type Alias Expense
+	aux := &struct {
+		ID string `json:"id"`
+		*Alias
+	}{
+		Alias: (*Alias)(e),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	e.ID, _ = primitive.ObjectIDFromHex(aux.ID)
+	return nil
 }
 
 func RegisterRoutes(r *gin.Engine) {
+	// Open DB connection and attaches it using the middleware
+	//re := gin.Default()
+	dbClient, _ := ConnectToDatabase()
+	r.Use(DatabaseMiddleware(dbClient))
+
 	// Define API endpoints here
 	r.GET("/expenses", GetExpenses)
 	r.POST("/expenses", CreateExpense)
 }
 
 func CreateExpense(c *gin.Context) {
-	// Connect to database
-	db, err := ConnectToDatabase()
-	if err != nil {
-		log.Printf("Error connecting to database %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Close connection database before returning
-	defer func(client *mongo.Client) {
-		err := Close(client)
-		if err != nil {
-			log.Printf("Error closing database connection: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-	}(db)
+	// Retrieve database connection
+	db, _ := c.MustGet("dbClient").(*mongo.Client)
 
 	// Decode the JSON request body
 	var expense Expense
 	decoder := json.NewDecoder(c.Request.Body)
-	err = decoder.Decode(&expense)
+	err := decoder.Decode(&expense)
 	if err != nil {
 		log.Printf("Error decoding JSON request body: %v", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -86,39 +106,40 @@ func CreateExpense(c *gin.Context) {
 }
 
 func GetExpenses(c *gin.Context) {
-	// Connect to database
-	db, err := ConnectToDatabase()
-	if err != nil {
-		log.Printf("Error connecting to database %v", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Close connection database before returning
-	defer func(client *mongo.Client) {
-		err := Close(client)
-		if err != nil {
-			log.Printf("Error closing database connection: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-	}(db)
+	// Retrieve database connection
+	db, _ := c.MustGet("dbClient").(*mongo.Client)
 
 	// Get the user's expenses
 	expenses, err := FindAllExpenses(db)
 	if err != nil {
-		log.Printf("Error getting expenses: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Encode the expenses as JSON
+	// Marshal the expenses to JSON
 	expensesJSON, err := json.Marshal(expenses)
 	if err != nil {
-		log.Printf("Error encoding expenses as JSON: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Respond to the request
-	c.JSON(http.StatusOK, expensesJSON)
+	// Decode the JSON expenses
+	decodedExpenses, err := DecodeJSONExpenses(expensesJSON)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error:": err.Error()})
+		return
+	}
+
+	//Respond to the request
+	c.JSON(http.StatusOK, decodedExpenses)
+}
+
+func DecodeJSONExpenses(expensesJSON []byte) ([]Expense, error) {
+	var expenses []Expense
+	err := json.Unmarshal(expensesJSON, &expenses)
+	if err != nil {
+		return nil, err
+	}
+
+	return expenses, nil
 }
--- LifeMetrics360-FinancialTracker-Backend/api/database.go
+++ LifeMetrics360-FinancialTracker-Backend/api/database.go
@@ -2,17 +2,36 @@ package api
 
 import (
 	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"os"
+	"time"
 )
+
+const (
+	DB_NAME                  = "life_metrics_360"
+	EXPENSES_COLLECTION_NAME = "expenses"
+)
+
+// DatabaseMiddleware sets up a Gin middleware that attaches the DB connection to the context of every incoming request.
+func DatabaseMiddleware(client *mongo.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("dbClient", client)
+		c.Next()
+	}
+}
 
 // ConnectToDatabase connects to the MongoDB database
 func ConnectToDatabase() (*mongo.Client, error) {
-	// TODO: Fix connection string for mongoDB
-	connectionString := "mongodb+srv://user_life_metrics_360:375au8uIvnreuWLw@cluster0.lsw8mua.mongodb.net/?retryWrites=true&w=majority"
+	//connectionString := "mongodb+srv://user_life_metrics_360:375au8uIvnreuWLw@lifemetrics360.dobldzn.mongodb.net/?retryWrites=true&w=majority"
+	connectionString := os.Getenv("MONGO_CONNECTION_STRING")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10) // 10 seconds timeout
+	defer cancel()
 
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connectionString))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
 	if err != nil {
 		log.Printf("Error connecting to database: %v", err)
 		return nil, err
@@ -23,7 +42,9 @@ func ConnectToDatabase() (*mongo.Client, error) {
 
 // Close closes the MongoDB database connection
 func Close(client *mongo.Client) error {
-	err := client.Disconnect(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10) // 10 seconds timeout
+	defer cancel()
+	err := client.Disconnect(ctx)
 	if err != nil {
 		log.Printf("Error closing database connection: %v", err)
 		return err
@@ -34,9 +55,11 @@ func Close(client *mongo.Client) error {
 
 // InsertExpense inserts a new expense record into the database.
 func InsertExpense(db *mongo.Client, expense Expense) error {
-	collection := db.Database("expenses").Collection("expenses")
+	collection := db.Database(DB_NAME).Collection(EXPENSES_COLLECTION_NAME)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10) // 10 seconds timeout
+	defer cancel()
 
-	_, err := collection.InsertOne(context.Background(), expense)
+	_, err := collection.InsertOne(ctx, expense)
 	if err != nil {
 		log.Printf("Error inserting expense record: %v", err)
 		return err
@@ -46,19 +69,21 @@ func InsertExpense(db *mongo.Client, expense Expense) error {
 }
 
 // FindAllExpenses gets all the expense records from the database
-func FindAllExpenses(db *mongo.Client) ([]Expense, error) {
-	collection := db.Database("expenses").Collection("expenses")
+func FindAllExpenses(client *mongo.Client) ([]Expense, error) {
+	collection := client.Database(DB_NAME).Collection(EXPENSES_COLLECTION_NAME)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10) // 10 seconds timeout
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
 
-	cursor, err := collection.Find(context.Background(), nil)
 	if err != nil {
 		log.Printf("Error getting expense records: %v", err)
 		return nil, err
 	}
 
 	var expenses []Expense
-	for cursor.Next(context.Background()) {
+	for cursor.Next(ctx) {
 		var expense Expense
-		err := cursor.Decode(&expenses)
+		err := cursor.Decode(&expense)
 		if err != nil {
 			log.Printf("Error decoding expense record: %v", err)
 			return nil, err
