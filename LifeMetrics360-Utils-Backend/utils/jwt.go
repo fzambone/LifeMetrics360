8d7307bfa44b5646394fb8e318a53055097183ee
fzambone <fernando.zambone@gmail.com>
2023-11-13 21:14:51 -0300

    Created UserManagement microservice

--- LifeMetrics360-Utils-Backend/utils/jwt.go
+++ LifeMetrics360-Utils-Backend/utils/jwt.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
 	"os"
 	"time"
 )
@@ -14,4 +15,14 @@ func GenerateToken(userID primitive.ObjectID) (string, error) {
 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+// HashPassword hashes a plain text password using bcrypt.
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
 }
