e7f6b32611e05f5314cfdc83c9e2a90ed6f91765
fernandozambone <fernando.zambone@gmail.com>
2023-11-20 21:09:45 -0600

    Creating Header component and adding a logo

--- Frontend/life-metrics-ui/src/App.js
+++ Frontend/life-metrics-ui/src/App.js
@@ -3,16 +3,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/' element={<SignIn />} />
-      </Routes>
-    </Router>
+    <div>
+      <Header/>
+      <Router>
+        <Routes>
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/' element={<SignIn />} />
+        </Routes>
+      </Router>
+    </div>
+
   );
 }
 
